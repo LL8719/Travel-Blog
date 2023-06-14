1fedc6cb9716e73a80a8be19bba03315142a62c4
LL8719 <arsgatslr_87@yahoo.com>
2023-06-13 21:08:36 -0700

    npm install

--- controllers/mailRoutes.js
+++ controllers/mailRoutes.js
@@ -14,12 +14,12 @@ const transporter = nodemailer.createTransport({
     pass: emailPassword,
   },
 });
-
+// Allows the user to share a post by generating a URL that includes the post's title and content, which can be copied and shared with others.
 router.get('/share', (req, res) => {
   const { postTitle, postContent } = req.query;
   res.render('share', { title: postTitle, content: postContent });
 });
-
+// extracts the values of 'postTitle', 'postContent', and 'userEmail' from the request's body to construct an email using "Nodemailer"
 router.post('/share-post', async (req, res) => {
   try {
     const { postTitle, postContent, userEmail } = req.body;
@@ -31,8 +31,9 @@ router.post('/share-post', async (req, res) => {
       subject: `Check out this post: ${postTitle}`,
       text: postContent,
     };
-
+    // The sendMail() method is an asynchronous function that sends an email and returns a Promise that resolves to an object containing information about the sent email.
     const info = await transporter.sendMail(mailOptions);
+
     console.log(`Email sent: ${info.response}`);
 
     res.redirect('/dashboard');
